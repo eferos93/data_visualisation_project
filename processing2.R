0c334f5f0d6d30e399dd31d1df74336541eb0a6e
eferos93 <eros.fabrici@gmail.com>
2021-11-24 16:47:27 +0100

    removed a category and refactor

--- processing2.R
+++ processing2.R
@@ -11,18 +11,20 @@ filteredDataset <-
 
 finalDataset <-
   filteredDataset %>%
-    filter(Professional.Status != "occupato" & Professional.Status != "non occupato" & Professional.Status != "totale") %>%
-    mutate(Professional.Status = case_when(
-      Professional.Status == "casalinga-o" ~ "homemaker",
-      Professional.Status == "direttivo, quadro, impiegato" ~ "BOD, employees",
-      Professional.Status == "dirigenti, imprenditori, liberi professionisti" ~ "managers, entrepreneur, freelancers",
-      Professional.Status == "disoccupato alla ricerca di nuova occupazione" ~ "unemployed looking for a job",
-      Professional.Status == "in altra condizione" ~ "other status",
-      Professional.Status == "in cerca di prima occupazione" ~ "unemployed looking for a job",
-      Professional.Status == "lavoratore in proprio, coadiuvante familiare, co.co.co." ~ "managers, entrepreneur, freelancers",
-      Professional.Status == "operaio, apprendista" ~ "worker, apprentice",
-      Professional.Status == "ritirato-a dal lavoro" ~ "other status",
-      Professional.Status == "studente" ~ "student"
+    filter(Professional.Status != "occupato" &
+             Professional.Status != "non occupato" &
+             Professional.Status != "totale",
+             Professional.Status != "lavoratore in proprio, coadiuvante familiare, co.co.co.") %>%
+    mutate(Professional.Status = recode(Professional.Status,
+                                        "casalinga-o" = "homemaker",
+                                        "direttivo, quadro, impiegato" = "BOD, employees",
+                                        "dirigenti, imprenditori, liberi professionisti" = "managers, entrepreneur, freelancers",
+                                        "disoccupato alla ricerca di nuova occupazione" = "unemployed looking for a job",
+                                        "in altra condizione" = "retired or in other status",
+                                        "in cerca di prima occupazione" = "unemployed looking for a job",
+                                        "operaio, apprendista" = "worker, apprentice",
+                                        "ritirato-a dal lavoro" = "retired or in other status",
+                                        "studente" = "student"
     )) %>%
     group_by(Year, Quarter, Professional.Status) %>%
     summarise(Percentage.People.Traveled = sum(Percentage.People.Traveled)) %>%
