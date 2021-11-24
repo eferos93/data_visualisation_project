575c10b7bb6aa053f152f4c6ac3e6fe1ce117113
eferos93 <eros.fabrici@gmail.com>
2021-11-24 20:15:01 +0100

    first draft plot

--- scripts/plots/linePlot.R
+++ scripts/plots/linePlot.R
@@ -1,9 +1,15 @@
 library(tidyverse)
 
+theme_set(theme_bw())
 data <- read.csv("processed_data/1.historical_data_1998_2013_type_of_journey.csv")
 
 data %>%
-  ggplot(aes(x = Year, y = Avg.People.Per.Quarter, group = Travel.Type , color = Travel.Type)) +
+  ggplot(aes(x = factor(Year), y = Avg.People.Per.Quarter, group = Travel.Type , color = Travel.Type)) +
   geom_line() +
-  geom_point() +
-  geom_errorbar(aes(ymin = Avg.People.Per.Quarter-Sd.People.Per.Quarter, ymax = Avg.People.Per.Quarter+Sd.People.Per.Quarter))
+  labs(
+    title = "Average Number of People every 100 that Traveled per Year",
+    caption = "Data Source: http:// dati.istat.it/",
+    x = "Year",
+    y = "Average number of people"
+  )
+  # geom_errorbar(aes(ymin = Avg.People.Per.Quarter-Sd.People.Per.Quarter, ymax = Avg.People.Per.Quarter+Sd.People.Per.Quarter))
