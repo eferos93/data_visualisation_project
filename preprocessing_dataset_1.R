18d15153bc977529e34bf44e3f4752ceab620495
eferos93 <eros.fabrici@gmail.com>
2021-11-24 16:14:54 +0100

    fix and refactor

--- preprocessing_dataset_1.R
+++ preprocessing_dataset_1.R
@@ -3,14 +3,18 @@ library(tidyr)
 
 data <- read.csv("./raw_data/1.historical_data_1998_2013_type_of_journey.csv", header = TRUE)
 
-data <- data %>% select("Tipo.di.viaggio", "TIME", "Value") %>%
-  separate("TIME", c("Year", "Quorter"), "-")  %>%
-  rename(Travel.Type = Tipo.di.viaggio) %>%
-  mutate(Travel.Type = recode(Travel.Type,
-                              "vacanza breve (1-3 notti)" = "short holiday (1-3 nights)",
-                              "vacanza lunga (4 o piÃ¹ notti)" = "long holiday (4 or more nights)",
-                              "viaggio di lavoro" = "business trip")) %>%
-  group_by_("Year", "Travel.Type") %>% summarise_at(vars("Value"), list(Avg = mean))
+processeData <-
+  data %>%
+    select(Tipo.di.viaggio, TIME, Value) %>%
+    separate(TIME, c("Year", "Quarter"), "-") %>%
+    rename(Travel.Type = Tipo.di.viaggio) %>%
+    mutate(Travel.Type = recode(Travel.Type,
+                                "vacanza breve (1-3 notti)" = "short holiday (1-3 nights)",
+                                "vacanza lunga (4 o più notti)" = "long holiday (4 or more nights)",
+                                "viaggio di lavoro" = "business trip")) %>%
+    group_by(Year, Travel.Type) %>%
+    summarise(Percentage.people.traveled = mean(Value)) %>%
+    ungroup()
 
-write.csv(data, "./processed_data/1.historical_data_1998_2013_type_of_journey.csv", row.names = FALSE)
+write.csv(processedData, "./processed_data/1.historical_data_1998_2013_type_of_journey.csv", row.names = FALSE)
 
