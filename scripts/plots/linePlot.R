206c2514eb8725bd61214e2a3278b51dde4d370a
eferos93 <eros.fabrici@gmail.com>
2021-11-27 17:55:34 +0100

    small refinement

--- scripts/plots/linePlot.R
+++ scripts/plots/linePlot.R
@@ -6,14 +6,15 @@ data <- read.csv("processed_data/1.historical_data_1998_2013_type_of_journey.csv
 data %>%
   ggplot(aes(x = factor(Year), y = Avg.People.Per.Quarter, group = Travel.Type)) +
   geom_line(aes(color = Travel.Type)) +
-  geom_vline(xintercept = 11, linetype = "longdash", color = "grey") +
-  geom_vline(xintercept = 15, linetype = "longdash", color = "grey") +
-  geom_text(aes(x = 10.5, label = "Finalcial Crisis", y = 7.5), color = "grey", size = 5) +
-  geom_text(aes(x = 14.5, label = "Spending Review", y = 5), color = "grey", size = 5) +
+  geom_vline(xintercept = 11, linetype = "longdash", color = "black", alpha = 0.5) +
+  geom_vline(xintercept = 15, linetype = "longdash", color = "black", alpha = 0.5) +
+  geom_text(aes(x = 10.5, label = "Finalcial Crisis", y = 7.5), color = "black", size = 5) +
+  geom_text(aes(x = 14.5, label = "Spending Review", y = 5), color = "black", size = 5) +
   labs(
     title = "Average Number of People every 100 that Traveled per Year",
     caption = "Data Source: http://dati.istat.it/",
     x = "Year",
-    y = "Average number of people"
+    y = "Average number of people",
+    color = ""
   )
   # geom_errorbar(aes(ymin = Avg.People.Per.Quarter-Sd.People.Per.Quarter, ymax = Avg.People.Per.Quarter+Sd.People.Per.Quarter))
